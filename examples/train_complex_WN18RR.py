e9e0f94aa15f4076af23217b258178c25803b5da
satyam.kumar.iiitv@gmail.com <satyam.kumar.iiitv@gmail.com>
2024-02-22 11:05:06 +0530

    Changes in train_Complex

--- examples/train_complex_WN18RR.py
+++ examples/train_complex_WN18RR.py
@@ -52,13 +52,17 @@ tester.run_triple_classification(threshlod=0.5)
 
 # Add RetrievalMAP metric
 retrieval_map = RetrievalMAP()
-print("Retrieval Map for WN188RR ", retrieval_map)
-with torch.no_grad():
-    complEx.eval()
-    for data in test_dataloader:
-        scores = complEx.predict(data)
-        print("Scores for WN18RR ",scores)
-        retrieval_map.update(scores, data)
-        #print("Retrieval Map for WN18RR ",retrieval_map)
-    map_value = retrieval_map.compute()
+# print("Retrieval Map for WN188RR ", retrieval_map)
+# with torch.no_grad():
+#     complEx.eval()
+#     for data in test_dataloader:
+#         scores = complEx.predict(data)
+#         print("Scores for WN18RR ",scores)
+#         retrieval_map.update(scores, data)
+#         #print("Retrieval Map for WN18RR ",retrieval_map)
+#     map_value = retrieval_map.compute()
+tester_output = tester.run_link_prediction(type_constrain=False)
+retrieval_map.update(tester_output)
+map_score = retrieval_map.compute()
+print("Retrieval MAP Score:", map_score)
 
