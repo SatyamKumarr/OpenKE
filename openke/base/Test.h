3067e41e9d69f14f18922a1482f76b55dfab9edd
satyam.kumar.iiitv@gmail.com <satyam.kumar.iiitv@gmail.com>
2024-02-22 11:26:21 +0530

    Changes in Test.h

--- openke/base/Test.h
+++ openke/base/Test.h
@@ -259,36 +259,36 @@ void test_link_prediction(bool type_constrain = false) {
 
     printf("no type constraint results:\n");
     
-    printf("metric:\t\t\t MRR \t\t MR \t\t hit@10 \t hit@3  \t hit@1 \n");
-    printf("l(raw):\t\t\t %f \t %f \t %f \t %f \t %f \n", l_reci_rank, l_rank, l_tot, l3_tot, l1_tot);
-    printf("r(raw):\t\t\t %f \t %f \t %f \t %f \t %f \n", r_reci_rank, r_rank, r_tot, r3_tot, r1_tot);
-    printf("averaged(raw):\t\t %f \t %f \t %f \t %f \t %f \n",
-            (l_reci_rank+r_reci_rank)/2, (l_rank+r_rank)/2, (l_tot+r_tot)/2, (l3_tot+r3_tot)/2, (l1_tot+r1_tot)/2);
+    printf("metric:\t\t\t MRR  \t hit@10 \t hit@3  \t hit@1 \n");
+    printf("l(raw):\t\t\t %f \t %f \t %f \n", l_reci_rank, l_tot, l1_tot);
+    printf("r(raw):\t\t\t %f \t %f \t %f \n", r_reci_rank, r_tot, r1_tot);
+    printf("averaged(raw):\t\t %f \t %f \t %f \n",
+            (l_reci_rank+r_reci_rank)/2, (l_tot+r_tot)/2, (l1_tot+r1_tot)/2);
     printf("\n");
-    printf("l(filter):\t\t %f \t %f \t %f \t %f \t %f \n", l_filter_reci_rank, l_filter_rank, l_filter_tot, l3_filter_tot, l1_filter_tot);
-    printf("r(filter):\t\t %f \t %f \t %f \t %f \t %f \n", r_filter_reci_rank, r_filter_rank, r_filter_tot, r3_filter_tot, r1_filter_tot);
-    printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
-            (l_filter_reci_rank+r_filter_reci_rank)/2, (l_filter_rank+r_filter_rank)/2, (l_filter_tot+r_filter_tot)/2, (l3_filter_tot+r3_filter_tot)/2, (l1_filter_tot+r1_filter_tot)/2);
+    // printf("l(filter):\t\t %f \t %f \t %f \t %f \t %f \n", l_filter_reci_rank, l_filter_rank, l_filter_tot, l3_filter_tot, l1_filter_tot);
+    // printf("r(filter):\t\t %f \t %f \t %f \t %f \t %f \n", r_filter_reci_rank, r_filter_rank, r_filter_tot, r3_filter_tot, r1_filter_tot);
+    // printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
+    //         (l_filter_reci_rank+r_filter_reci_rank)/2, (l_filter_rank+r_filter_rank)/2, (l_filter_tot+r_filter_tot)/2, (l3_filter_tot+r3_filter_tot)/2, (l1_filter_tot+r1_filter_tot)/2);
 
     mrr = (l_filter_reci_rank+r_filter_reci_rank) / 2;
-    mr = (l_filter_rank+r_filter_rank) / 2;
+    // mr = (l_filter_rank+r_filter_rank) / 2;
     hit10 = (l_filter_tot+r_filter_tot) / 2;
-    hit3 = (l3_filter_tot+r3_filter_tot) / 2;
+    // hit3 = (l3_filter_tot+r3_filter_tot) / 2;
     hit1 = (l1_filter_tot+r1_filter_tot) / 2;
 
     if (type_constrain) {
         //type constrain
-        l_rank_constrain /= testTotal;
-        r_rank_constrain /= testTotal;
+        // l_rank_constrain /= testTotal;
+        // r_rank_constrain /= testTotal;
         l_reci_rank_constrain /= testTotal;
         r_reci_rank_constrain /= testTotal;
      
         l_tot_constrain /= testTotal;
-        l3_tot_constrain /= testTotal;
+        // l3_tot_constrain /= testTotal;
         l1_tot_constrain /= testTotal;
      
         r_tot_constrain /= testTotal;
-        r3_tot_constrain /= testTotal;
+        // r3_tot_constrain /= testTotal;
         r1_tot_constrain /= testTotal;
 
         // with filter
@@ -307,50 +307,71 @@ void test_link_prediction(bool type_constrain = false) {
 
         printf("type constraint results:\n");
         
-        printf("metric:\t\t\t MRR \t\t MR \t\t hit@10 \t hit@3  \t hit@1 \n");
-        printf("l(raw):\t\t\t %f \t %f \t %f \t %f \t %f \n", l_reci_rank_constrain, l_rank_constrain, l_tot_constrain, l3_tot_constrain, l1_tot_constrain);
-        printf("r(raw):\t\t\t %f \t %f \t %f \t %f \t %f \n", r_reci_rank_constrain, r_rank_constrain, r_tot_constrain, r3_tot_constrain, r1_tot_constrain);
-        printf("averaged(raw):\t\t %f \t %f \t %f \t %f \t %f \n",
-                (l_reci_rank_constrain+r_reci_rank_constrain)/2, (l_rank_constrain+r_rank_constrain)/2, (l_tot_constrain+r_tot_constrain)/2, (l3_tot_constrain+r3_tot_constrain)/2, (l1_tot_constrain+r1_tot_constrain)/2);
+        // printf("metric:\t\t\t MRR \t\t MR \t\t hit@10 \t hit@3  \t hit@1 \n");
+        // printf("l(raw):\t\t\t %f \t %f \t %f \t %f \t %f \n", l_reci_rank_constrain, l_rank_constrain, l_tot_constrain, l3_tot_constrain, l1_tot_constrain);
+        // printf("r(raw):\t\t\t %f \t %f \t %f \t %f \t %f \n", r_reci_rank_constrain, r_rank_constrain, r_tot_constrain, r3_tot_constrain, r1_tot_constrain);
+        // printf("averaged(raw):\t\t %f \t %f \t %f \t %f \t %f \n",
+        //         (l_reci_rank_constrain+r_reci_rank_constrain)/2, (l_rank_constrain+r_rank_constrain)/2, (l_tot_constrain+r_tot_constrain)/2, (l3_tot_constrain+r3_tot_constrain)/2, (l1_tot_constrain+r1_tot_constrain)/2);
+        // printf("\n");
+        // printf("l(filter):\t\t %f \t %f \t %f \t %f \t %f \n", l_filter_reci_rank_constrain, l_filter_rank_constrain, l_filter_tot_constrain, l3_filter_tot_constrain, l1_filter_tot_constrain);
+        // printf("r(filter):\t\t %f \t %f \t %f \t %f \t %f \n", r_filter_reci_rank_constrain, r_filter_rank_constrain, r_filter_tot_constrain, r3_filter_tot_constrain, r1_filter_tot_constrain);
+        // printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
+        //         (l_filter_reci_rank_constrain+r_filter_reci_rank_constrain)/2, (l_filter_rank_constrain+r_filter_rank_constrain)/2, (l_filter_tot_constrain+r_filter_tot_constrain)/2, (l3_filter_tot_constrain+r3_filter_tot_constrain)/2, (l1_filter_tot_constrain+r1_filter_tot_constrain)/2);
+        printf("metric:\t\t\t MRR  \t hit@10 \t hit@3  \t hit@1 \n");
+        printf("l(raw):\t\t\t %f \t %f \t %f \n", l_reci_rank, l_tot, l1_tot);
+        printf("r(raw):\t\t\t %f \t %f \t %f \n", r_reci_rank, r_tot, r1_tot);
+        printf("averaged(raw):\t\t %f \t %f \t %f \n",
+                (l_reci_rank+r_reci_rank)/2, (l_tot+r_tot)/2, (l1_tot+r1_tot)/2);
         printf("\n");
-        printf("l(filter):\t\t %f \t %f \t %f \t %f \t %f \n", l_filter_reci_rank_constrain, l_filter_rank_constrain, l_filter_tot_constrain, l3_filter_tot_constrain, l1_filter_tot_constrain);
-        printf("r(filter):\t\t %f \t %f \t %f \t %f \t %f \n", r_filter_reci_rank_constrain, r_filter_rank_constrain, r_filter_tot_constrain, r3_filter_tot_constrain, r1_filter_tot_constrain);
-        printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
-                (l_filter_reci_rank_constrain+r_filter_reci_rank_constrain)/2, (l_filter_rank_constrain+r_filter_rank_constrain)/2, (l_filter_tot_constrain+r_filter_tot_constrain)/2, (l3_filter_tot_constrain+r3_filter_tot_constrain)/2, (l1_filter_tot_constrain+r1_filter_tot_constrain)/2);
+        // printf("l(filter):\t\t %f \t %f \t %f \t %f \t %f \n", l_filter_reci_rank, l_filter_rank, l_filter_tot, l3_filter_tot, l1_filter_tot);
+        // printf("r(filter):\t\t %f \t %f \t %f \t %f \t %f \n", r_filter_reci_rank, r_filter_rank, r_filter_tot, r3_filter_tot, r1_filter_tot);
+        // printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
+        //         (l_filter_reci_rank+r_filter_reci_rank)/2, (l_filter_rank+r_filter_rank)/2, (l_filter_tot+r_filter_tot)/2, (l3_filter_tot+r3_filter_tot)/2, (l1_filter_tot+r1_filter_tot)/2);
 
         mrrTC = (l_filter_reci_rank_constrain+r_filter_reci_rank_constrain)/2;
-        mrTC = (l_filter_rank_constrain+r_filter_rank_constrain) / 2;
+        //mrTC = (l_filter_rank_constrain+r_filter_rank_constrain) / 2;
         hit10TC = (l_filter_tot_constrain+r_filter_tot_constrain) / 2;
-        hit3TC = (l3_filter_tot_constrain+r3_filter_tot_constrain) / 2;
+        //hit3TC = (l3_filter_tot_constrain+r3_filter_tot_constrain) / 2;
         hit1TC = (l1_filter_tot_constrain+r1_filter_tot_constrain) / 2;
     }
 }
 
 extern "C"
 void test_relation_prediction() {
-    rel_rank /= testTotal;
+   // rel_rank /= testTotal;
     rel_reci_rank /= testTotal;
   
     rel_tot /= testTotal;
-    rel3_tot /= testTotal;
+    //rel3_tot /= testTotal;
     rel1_tot /= testTotal;
 
     // with filter
-    rel_filter_rank /= testTotal;
+    //rel_filter_rank /= testTotal;
     rel_filter_reci_rank /= testTotal;
   
     rel_filter_tot /= testTotal;
-    rel3_filter_tot /= testTotal;
+    //rel3_filter_tot /= testTotal;
     rel1_filter_tot /= testTotal;
 
     printf("no type constraint results:\n");
     
-    printf("metric:\t\t\t MRR \t\t MR \t\t hit@10 \t hit@3  \t hit@1 \n");
-    printf("averaged(raw):\t\t %f \t %f \t %f \t %f \t %f \n",
-            rel_reci_rank, rel_rank, rel_tot, rel3_tot, rel1_tot);
+    // printf("metric:\t\t\t MRR \t\t MR \t\t hit@10 \t hit@3  \t hit@1 \n");
+    // printf("averaged(raw):\t\t %f \t %f \t %f \t %f \t %f \n",
+    //         rel_reci_rank, rel_rank, rel_tot, rel3_tot, rel1_tot);
+    // printf("\n");
+    // printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
+    //         rel_filter_reci_rank, rel_filter_rank, rel_filter_tot, rel3_filter_tot, rel1_filter_tot);
+    printf("metric:\t\t\t MRR  \t hit@10 \t hit@3  \t hit@1 \n");
+    printf("l(raw):\t\t\t %f \t %f \t %f \n", l_reci_rank, l_tot, l1_tot);
+    printf("r(raw):\t\t\t %f \t %f \t %f \n", r_reci_rank, r_tot, r1_tot);
+    printf("averaged(raw):\t\t %f \t %f \t %f \n",
+            (l_reci_rank+r_reci_rank)/2, (l_tot+r_tot)/2, (l1_tot+r1_tot)/2);
     printf("\n");
-    printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
-            rel_filter_reci_rank, rel_filter_rank, rel_filter_tot, rel3_filter_tot, rel1_filter_tot);
+    // printf("l(filter):\t\t %f \t %f \t %f \t %f \t %f \n", l_filter_reci_rank, l_filter_rank, l_filter_tot, l3_filter_tot, l1_filter_tot);
+    // printf("r(filter):\t\t %f \t %f \t %f \t %f \t %f \n", r_filter_reci_rank, r_filter_rank, r_filter_tot, r3_filter_tot, r1_filter_tot);
+    // printf("averaged(filter):\t %f \t %f \t %f \t %f \t %f \n",
+    //         (l_filter_reci_rank+r_filter_reci_rank)/2, (l_filter_rank+r_filter_rank)/2, (l_filter_tot+r_filter_tot)/2, (l3_filter_tot+r3_filter_tot)/2, (l1_filter_tot+r1_filter_tot)/2);
+
 }
 
 extern "C"
@@ -361,12 +382,12 @@ REAL getTestLinkHit10(bool type_constrain = false) {
     return hit10;
 }
 
-extern "C"
-REAL  getTestLinkHit3(bool type_constrain = false) {
-    if (type_constrain)
-        return hit3TC;
-    return hit3;
-}
+// extern "C"
+// REAL  getTestLinkHit3(bool type_constrain = false) {
+//     if (type_constrain)
+//         return hit3TC;
+//     return hit3;
+// }
 
 extern "C"
 REAL  getTestLinkHit1(bool type_constrain = false) {
@@ -375,12 +396,12 @@ REAL  getTestLinkHit1(bool type_constrain = false) {
     return hit1;
 }
 
-extern "C"
-REAL  getTestLinkMR(bool type_constrain = false) {
-    if (type_constrain)
-        return mrTC;
-    return mr;
-}
+// extern "C"
+// REAL  getTestLinkMR(bool type_constrain = false) {
+//     if (type_constrain)
+//         return mrTC;
+//     return mr;
+// }
 
 extern "C"
 REAL  getTestLinkMRR(bool type_constrain = false) {
